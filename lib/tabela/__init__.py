87b0c12967fcadd0fc95805edf5d54900343c881
Pedro Paulo <pedropaulodfr@outlook.com>
2023-02-11 11:52:38 -0300

    Corrigido problema na rota /tabela

--- lib/tabela/__init__.py
+++ lib/tabela/__init__.py
@@ -42,8 +42,12 @@ def raspagem_tabela():
             tabela[i].find_all("td")[11].find_all("span")[1].text, 
             tabela[i].find_all("td")[11].find_all("span")[2].text
             ])
-        proximo_jogo = tabela[i].find_all("img", class_="icon escudo")[0].get("title")
-        escudo = tabela[i].find_all("img", class_="icon escudo m-r-10")[0].get("src")
+        try:
+            proximo_jogo = tabela[i].find_all("img", class_="icon escudo")[0].get("title")
+            escudo = tabela[i].find_all("img", class_="icon escudo m-r-10")[0].get("src")
+        except:
+            proximo_jogo = "Sem Imagem"
+            escudo = "https://triunfo.pe.gov.br/pm_tr430/wp-content/uploads/2018/03/sem-foto.jpg" 
 
         dados.append([
             posicao,
